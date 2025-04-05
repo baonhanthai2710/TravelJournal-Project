1ad3890c256bfc60449a9fee55c9f45e5f80053d
baonhanthai2710 <baonhanthai2710@gmail.com>
2025-04-05 21:13:36 +0700

    fix(ui): navbar height

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ import { FaGlobeAmericas } from "react-icons/fa";
 export default function Header () {
     return (
         <header className="header-container">
-            <FaGlobeAmericas className="globe-icon"/>
+            <FaGlobeAmericas className="globe-icon" alt="globe-icon"/>
             <h1>my travel journal.</h1>
         </header>
     )
