0370da36d59d0ec9d144d8d01ba69f201824ca83
baonhanthai2710 <baonhanthai2710@gmail.com>
2025-04-07 20:17:34 +0700

    fix(data): map entry component

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,26 @@
 import Header from "./components/Header"
 import Entry from "./components/Entry"
-import MountFujiImg from "./assets/mount-fuji.png"
+import TravelData from "./data"
 
 export default function App() {
+  
+  const travelElements = TravelData.map((travel) => {
+    return <Entry 
+              img = {travel.img}
+              alt = {travel.alt}
+              title = {travel.title}
+              country = {travel.country}
+              googleMapsLink = {travel.googleMapsLink}
+              dates = {travel.dates}
+              text = {travel.text}
+    /> 
+  })
+
   return (
     <>
       <Header />      
       <main className="container">
-        <Entry 
-          img = {MountFujiImg}
-          alt = "mount-fuji-img"
-          title = "Mount Fuji"
-          country = "Japan"
-          googleMapsLink = "https://www.google.com/maps/place/Mount+Fuji/@35.3606421,138.7170637,15z/data=!3m1!4b1!4m6!3m5!1s0x6019629a42fdc899:0xa6a1fcc916f3a4df!8m2!3d35.3606255!4d138.7273634!16zL20vMGNrczA?entry=ttu"
-          dates = "12 Jan, 2021 - 24 Jan, 2021"
-          text = "Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists."
-        />
+          {travelElements}
       </main>
     </>
   )
